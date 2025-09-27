64fd6872b53de2171b105e892afe4cf4ef7e996a
Alberto Filipe <albertoofilipe@gmail.com>
2025-09-27 21:43:11 +0100

    Ex de Dados

---
+++ inicio/dado3.js
@@ -0,0 +1,13 @@
+let idade = 31
+console.log(typeof idade)
+
+let salario = 4567.32
+console.log(typeof salario)
+
+let estaChovendo = false
+console.log(typeof estaChovendo)
+
+let texto = "Teste"
+console.log(typeof texto)
+
+console.log(typeof null)
