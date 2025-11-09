24c87ec1e018e0e48e65eec057a133b18337e30e
Alberto Filipe <albertoofilipe@gmail.com>
2025-11-09 13:58:59 +0000

    ex JS

---
+++ fundamentos/arrays.js
@@ -0,0 +1,17 @@
+const valores = [7.7, 8.9, 6.3, 9.2]
+console.log(valores[0])
+
+valores[4] = 10
+
+console.log(valores)
+console.log(valores.length)
+
+valores.push({id: 3}, null, 'olá')
+valores.pop()
+
+console.log(valores)
+
+delete valores[0]
+console.log(valores)
+
+console.log(typeof valores)
