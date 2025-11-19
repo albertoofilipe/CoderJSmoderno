84153c02e3ddc913c89429afd2fd3a5887238a8c
Alberto Filipe <albertoofilipe@gmail.com>
2025-11-19 11:32:58 +0000

    Ex JS

---
+++ fundamentos/aritrimeticos.js
@@ -0,0 +1,9 @@
+const [n1, n2, n3, n4] = [3, 5, 1, 15]
+
+const soma = n1 + n2 + n3 + n4
+const subtracao = n1 - n3
+const multiplicacao = n1 * n4
+const divisao = n2 / n1
+const modulo = n4 % 2
+
+console.log(soma, subtracao, multiplicacao, divisao.toFixed(2 ), modulo)
---
+++ fundamentos/destructuring04.js
@@ -0,0 +1,11 @@
+function rand([min = 0, max = 1000] = []) {
+    if (min > max) [min, max] = [max, min]
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+console.log(rand([10, 0]))
+console.log(rand([150]))
+console.log(rand([ ,350]))
+console.log(rand([]))
+console.log(rand())
