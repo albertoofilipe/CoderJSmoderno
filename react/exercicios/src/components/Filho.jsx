a6f5f3b494ec5cdfabb994e50e76435a0e4fbe57
Alberto Filipe <albertoofilipe@gmail.com>
2026-05-01 14:15:38 +0100

    aulas de react

---
+++ react/exercicios/src/components/Filho.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+import Pai from "./Pai"
+
+export default props => <li>{props.nome} {props.sobrenome}</li>
--- react/exercicios/src/main.jsx
+++ react/exercicios/src/main.jsx
@@ -1,8 +1,16 @@
-import { React } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 
-import BomDia from './components/BomDia'
+import Pai from './components/Pai'
+import Filho from './components/Filho'
 
-
-createRoot(document.getElementById('root')).render(<BomDia nome="Alberto" />)
+createRoot(document.getElementById('root')).render(
+    <div>
+        <Pai nome="Alberto" sobrenome="Filipe">
+            <Filho nome="Anaize" />
+            <Filho nome="Adalberto" sobrenome="Filipe" />
+            <Filho nome="Kaká" sobrenome="Filipe" />
+        </Pai>
+    </div>
+)
 
