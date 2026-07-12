cbff350f94f374abf34393ba1e6b85a6bb837ba6
Alberto Filipe <albertoofilipe@gmail.com>
2026-07-12 13:51:07 +0100

    next

--- react/crud/frontend/src/components/template/Nav.jsx
+++ react/crud/frontend/src/components/template/Nav.jsx
@@ -3,5 +3,13 @@ import React from 'react'
 
 export default props => 
     <aside className="menu-area">
-        Navegação
+        <nav className="menu">
+            {/* Refactorar em casa*/}
+            <a href="#/">
+                <i className="fa fa-home"> Início</i>
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"> Início</i>
+            </a>
+        </nav>
     </aside>
